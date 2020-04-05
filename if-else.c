ba71cf20f2aed4c8873ad2ace23a13c61cc8e2f0
pankaj4653 <pankaj.k4653@gmail.com>
2020-04-05 23:51:16 +0530

    If program in C

---
+++ if-else.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  int num;
+  printf("Enter a number less than 10 : ");
+  scanf("%d",&num);
+
+  if (num<= 10)
+    printf("What an obedient servent you are!");
+}
